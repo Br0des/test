f13ac1da894ab7e189c4bcb103bba99706f4af3a
Brodes <144500576+Br0des@users.noreply.github.com>
2025-03-17 18:20:14 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// This is a placeholder for any JavaScript functionality you may want to add
+document.querySelectorAll('.product button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Added to cart!');
+    });
+});
